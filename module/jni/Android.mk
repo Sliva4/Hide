4268ecc012db92bce171d2583ba110284b6bf2aa
Sliva4 <slivaktoto@gmail.com>
2025-12-13 22:55:21 +0700

    Testing

--- module/jni/Android.mk
+++ module/jni/Android.mk
@@ -1,8 +1,8 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := example
-LOCAL_SRC_FILES := example.cpp
+LOCAL_MODULE := ZygiskHide
+LOCAL_SRC_FILES := main.cpp local_cxa_atexit_finalize_impl/atexit.cpp
 LOCAL_STATIC_LIBRARIES := libcxx
 LOCAL_LDLIBS := -llog
 include $(BUILD_SHARED_LIBRARY)
