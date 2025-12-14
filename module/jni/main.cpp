cf84fd6e3b7f0101f8c4a160df0134ece2f2e53a
Sliva4 <slivaktoto@gmail.com>
2025-12-14 09:59:05 +0700

    Fix

--- module/jni/main.cpp
+++ module/jni/main.cpp
@@ -23,7 +23,7 @@ using zygisk::Api;
 using zygisk::AppSpecializeArgs;
 using zygisk::ServerSpecializeArgs;
 
-#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, "MyModule", __VA_ARGS__)
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, "ZygiskHide", __VA_ARGS__)
 
 class MyModule : public zygisk::ModuleBase {
 public:
